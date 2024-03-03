9e31445ec069760fe7996ac85383ff5768d048f9
Karaby <fablelite11@gmail.com>
2024-03-03 17:56:39 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    //Задача 1
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(i);
+        }
+        //Задача 2
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        //Задача 3
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+        //Задача 4
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        //Задача 5
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+        //Задача 6
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        //Задача 7
+        for (int i = 1; i<= 512; i = i * 2) {
+            System.out.println(i);
+        }
+        //Задача 8
+
+    }
+    }
